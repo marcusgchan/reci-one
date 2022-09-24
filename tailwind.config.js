16810c50e764bfc8e0a20ab7ac873d8f40f35a94
marcusgchan <marcusgchan@users.noreply.github.com>
2022-09-24 00:36:06 +0000

    Prettified Code!

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,4 @@
-const defaultTheme = require('tailwindcss/defaultTheme')
+const defaultTheme = require("tailwindcss/defaultTheme");
 
 /** @type {import('tailwindcss').Config} */
 
@@ -7,31 +7,30 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        'accent-500': "#B48714",
-        'accent-400': "#D6B665",
-        'accent-300': '#E0C98C',
+        "accent-500": "#B48714",
+        "accent-400": "#D6B665",
+        "accent-300": "#E0C98C",
         primary: "#171717",
         secondary: "#fafafa",
       },
       borderWidth: {
-        '3': '3px'
+        3: "3px",
       },
       animation: {
-        'fade-in-down': 'fade-in-down 300ms ease-in forwards',
+        "fade-in-down": "fade-in-down 300ms ease-in forwards",
       },
       keyframes: {
-        'fade-in-down': {
-          '0%': {
+        "fade-in-down": {
+          "0%": {
             opacity: 0,
-            transform: 'translate(0, -10px)'
+            transform: "translate(0, -10px)",
           },
-          '100%': {
+          "100%": {
             opacity: 1,
-            transform: 'translate(0, 0px)'
+            transform: "translate(0, 0px)",
           },
-
-        }
-      }
+        },
+      },
     },
   },
   plugins: [],
