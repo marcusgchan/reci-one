05dd62892e9185665e04ef68421e12db253a51f3
Marcus Chan <marcusgchan35@gmail.com>
2022-09-23 17:32:34 -0700

    rw-12: refactored to service

--- src/server/router/recipes.ts
+++ src/server/router/recipes.ts
@@ -1,7 +1,8 @@
 import { createRouter } from "./context";
 import { z } from "zod";
-import { getRecipesSchema, GetRecipesQuery } from "../../shared/schemas/recipe";
+import { getRecipesSchema } from "../../shared/schemas/recipe";
 import { TRPCError } from "@trpc/server";
+import { recipeService } from "../services/recipesService";
 
 export const recipesRouter = createRouter()
   .query("hello", {
@@ -21,69 +22,6 @@ export const recipesRouter = createRouter()
     async resolve({ ctx, input }) {
       const userId = ctx.session?.user?.id;
       if (!userId) throw new TRPCError({ code: "UNAUTHORIZED" });
-      const recipes = await ctx.prisma.recipe.findMany({
-        where: {
-          authorId: "cl7b4r5n20216kb6vj9fucbt1",
-          name: {
-            contains: input.search,
-          },
-          ingredients: getIncludeExcludeItems(
-            "name",
-            "ingredientsInclude",
-            "ingredientsExclude",
-            input
-          ),
-          nationalities: getIncludeExcludeItems(
-            "name",
-            "nationalitiesInclude",
-            "nationalitiesExclude",
-            input
-          ),
-          prepTime: {
-            gt: input.filters.prepTimeMin,
-            lt: input.filters.prepTimeMax,
-          },
-          cookTime: {
-            gt: input.filters.cookTimeMin,
-            lt: input.filters.cookTimeMax,
-          },
-        },
-      });
-
-      console.log(recipes);
-      return recipes;
+      return await recipeService.getMyRecipes(ctx, userId, input);
     },
   });
-
-type FilterFields =
-  | "ingredientsInclude"
-  | "ingredientsExclude"
-  | "nationalitiesInclude"
-  | "nationalitiesExclude";
-
-function getIncludeExcludeItems(
-  columnName: string,
-  includeFilterField: FilterFields,
-  excludeFilterField: FilterFields,
-  input: GetRecipesQuery
-) {
-  const {
-    [includeFilterField]: includeList,
-    [excludeFilterField]: excludeList,
-  } = input.filters;
-  return {
-    some:
-      includeList.length > 0
-        ? {
-            OR: includeList.map((ingredient) => ({
-              [columnName]: { contains: ingredient },
-            })),
-          }
-        : {},
-    none: {
-      OR: excludeList.map((ingredient) => ({
-        [columnName]: { contains: ingredient },
-      })),
-    },
-  };
-}
---
+++ src/server/services/recipesService.ts
@@ -0,0 +1,74 @@
+import { GetRecipesQuery } from "../../shared/schemas/recipe";
+import { Context } from "../router/context";
+
+export const recipeService = {
+  getMyRecipes,
+};
+
+async function getMyRecipes(
+  ctx: Context,
+  userId: string,
+  input: GetRecipesQuery
+) {
+  return await ctx.prisma.recipe.findMany({
+    where: {
+      authorId: userId, // Replace with "cl7b4r5n20216kb6vj9fucbt1" for testing
+      name: {
+        contains: input.search,
+      },
+      ingredients: getIncludeExcludeItems(
+        "name",
+        "ingredientsInclude",
+        "ingredientsExclude",
+        input
+      ),
+      nationalities: getIncludeExcludeItems(
+        "name",
+        "nationalitiesInclude",
+        "nationalitiesExclude",
+        input
+      ),
+      prepTime: {
+        gt: input.filters.prepTimeMin,
+        lt: input.filters.prepTimeMax,
+      },
+      cookTime: {
+        gt: input.filters.cookTimeMin,
+        lt: input.filters.cookTimeMax,
+      },
+    },
+  });
+}
+
+function getIncludeExcludeItems(
+  columnName: string,
+  includeFilterField: FilterFields,
+  excludeFilterField: FilterFields,
+  input: GetRecipesQuery
+) {
+  const {
+    [includeFilterField]: includeList,
+    [excludeFilterField]: excludeList,
+  } = input.filters;
+  return {
+    some:
+      includeList.length > 0
+        ? {
+            OR: includeList.map((ingredient) => ({
+              [columnName]: { contains: ingredient },
+            })),
+          }
+        : {},
+    none: {
+      OR: excludeList.map((ingredient) => ({
+        [columnName]: { contains: ingredient },
+      })),
+    },
+  };
+}
+
+type FilterFields =
+  | "ingredientsInclude"
+  | "ingredientsExclude"
+  | "nationalitiesInclude"
+  | "nationalitiesExclude";
