c28256fca060194b67d61bc6dd94e741a3499e7d
Marcus <marcuscchan@gmail.com>
2022-11-12 19:27:26 -0800

    added background color

--- src/pages/recipes/create.tsx
+++ src/pages/recipes/create.tsx
@@ -601,7 +601,7 @@ const SearchableSelect = ({
         </Combobox.Button>
 
         {filteredData.length > 0 && (
-          <Combobox.Options className="absolute z-10 mt-1 max-h-60 w-full overflow-auto rounded-md py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 sm:text-sm">
+          <Combobox.Options className="absolute z-10 mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 sm:text-sm">
             {filteredData.map((data) => (
               <Combobox.Option
                 key={data.id}
