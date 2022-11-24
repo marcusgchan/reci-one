dcdc1137a88927f4519829e4fa1748a771a4e579
Marcus Chan <marcusgchan35@gmail.com>
2022-11-23 19:52:18 -0800

    updated env to avoid conflict with vercel and removed unused keyfile env

--- src/utils/s3Client.ts
+++ src/utils/s3Client.ts
@@ -1,7 +1,14 @@
 // Create service client module using ES6 syntax.
 import { S3Client } from "@aws-sdk/client-s3";
+import { env } from "src/server/env.mjs";
 // Set the AWS Region.
 const REGION = "us-west-1";
 // Create an Amazon S3 service client object.
-const s3Client = new S3Client({ region: REGION });
+const s3Client = new S3Client({
+  region: REGION,
+  credentials: {
+    accessKeyId: env.ACCESS_KEY_ID,
+    secretAccessKey: env.SECRET_ACCESS_KEY,
+  },
+});
 export { s3Client };
