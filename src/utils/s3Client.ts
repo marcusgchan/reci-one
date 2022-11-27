8830faf13af5ee8f92f515b73516078903858bca
Marcus Chan <marcusgchan35@gmail.com>
2022-11-26 19:20:01 -0800

    update s3 client to work in prod

--- src/utils/s3Client.ts
+++ src/utils/s3Client.ts
@@ -1,16 +1,19 @@
 // Create service client module using ES6 syntax.
 import { S3Client } from "@aws-sdk/client-s3";
 import { env } from "src/server/env.mjs";
+
 // Set the AWS Region.
 const REGION = "us-west-1";
+const isDevelopmentMode = env.NODE_ENV === "development" || "test";
 // Create an Amazon S3 service client object.
 const s3Client = new S3Client({
-  endpoint: env.BUCKET_DOMAIN,
+  endpoint: isDevelopmentMode ? env.BUCKET_DOMAIN : undefined,
   region: REGION,
-  forcePathStyle: env.NODE_ENV === "development" || "test" ? true : false,
+  forcePathStyle: isDevelopmentMode ? true : false,
   credentials: {
     accessKeyId: env.ACCESS_KEY_ID,
     secretAccessKey: env.SECRET_ACCESS_KEY,
   },
 });
+
 export { s3Client };
