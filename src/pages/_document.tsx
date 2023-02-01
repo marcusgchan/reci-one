91363bc03b85169c24262e1409f9ce803238e03a
Marcus Chan <marcuscchan@gmail.com>
2023-01-31 22:07:36 -0800

    Fixed scrollbar issue

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,7 +6,7 @@ export default function Document() {
       <Head>
         <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
       </Head>
-      <body className="h-full">
+      <body className="min-h-full">
         <Main />
         <NextScript />
       </body>
