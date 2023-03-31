b1f7afa504eea9cbe9197646a6bdfb8c2ec9487e
tfu2003 <officialtonyfu@gmail.com>
2023-03-30 21:58:58 -0700

    RW-25: Create recipe page

--- src/pages/recipes/[recipeId].tsx
+++ src/pages/recipes/[recipeId].tsx
@@ -7,8 +7,30 @@ export default function Recipe() {
   const { data, isError, isLoading } = trpc.recipes.getRecipe.useQuery({
     recipeId: String(router.query.recipeId),
   });
+
   if (isLoading) {
     return <Loader />;
   }
-  return <div>Unfinished</div>;
+
+  return (
+    <div>
+      {data?.name}
+      {data?.description}
+      {data?.ingredients.map((id, key) => (
+        <div key={key}> {id.name}</div>
+      ))}
+      {data?.steps.map((id, key) => (
+        <div key={key}> {id.name}</div>
+      ))}
+      {data?.mealTypes.map((id, key) => (
+        <div key={key}> {id.mealTypeId}</div>
+      ))}
+      {data?.cookingMethods.map((id, key) => (
+        <div key={key}> {id.cookingMethodId}</div>
+      ))}
+      {data?.nationalities.map((id, key) => (
+        <div key={key}> {id.nationalityId}</div>
+      ))}
+    </div>
+  );
 }
