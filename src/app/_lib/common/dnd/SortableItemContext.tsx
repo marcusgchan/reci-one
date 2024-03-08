876ccd6b2b6cdfdfc61c28c4ed631833492c7e1f
Marcus Chan <marcuscchan@gmail.com>
2024-03-08 14:29:13 -0800

    Fix more linting problems

--- src/app/_lib/common/dnd/SortableItemContext.tsx
+++ src/app/_lib/common/dnd/SortableItemContext.tsx
@@ -2,7 +2,8 @@ import { createContext, useContext } from "react";
 import { type DraggableSyntheticListeners } from "@dnd-kit/core";
 
 interface Context {
-  attributes: Record<string, unknown>;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  attributes: Record<string, any>;
   listeners: DraggableSyntheticListeners;
   ref(node: HTMLElement | null): void;
 }
